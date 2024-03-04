79a5f3cce48dac88f5d6258e8818a8223235e1aa
Nikolai <borodinov.nikolai@gmail.com>
2024-03-04 23:57:52 +0300

    dropdown done

--- front/src/widgets/header/ui/index.tsx
+++ front/src/widgets/header/ui/index.tsx
@@ -9,8 +9,9 @@ import Svg_profile from '@/public/icons/profile.svg'
 import Svg_favorites from '@/public/icons/favorites.svg'
 import Svg_cart from '@/public/icons/cart.svg'
 import { useGetUserQuery } from '@/src/entities/user'
-import MobileNavabr from './mobileNavbar'
+import MobileNavabr from './components/mobileNavbar'
 import Drawer from '@/src/shared/ui/drawer'
+import DropdownMenu from './components/dropdownMenu'
 
 
 
@@ -52,7 +53,11 @@ const {data: userData, isSuccess} = useGetUserQuery()
         <div className={styles.menu_inner + ' container'} >
             <div className={styles.burger} >
           <Drawer content={<MobileNavabr/>} >
-              <Image src={'/icons/burger.png'} width={36} height={8} alt='burger icon' />
+          <svg width="37" height="11" viewBox="0 0 37 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<line x1="36.0878" y1="1.96484" x2="11.0878" y2="1.96484" stroke="black" stroke-width="2"/>
+<line x1="36.0878" y1="9.96484" x2="0.0877685" y2="9.96484" stroke="black" stroke-width="2"/>
+</svg>
+
               <p className={styles.burger_text} >Меню</p>
           </Drawer>
             </div>
@@ -63,9 +68,14 @@ const {data: userData, isSuccess} = useGetUserQuery()
         
           <ul className={styles.categories} >
             <li className={styles.catalog} >
-              <Image src={'/icons/burger.png'} width={36} height={8} alt='burger icon' />
-              Каталог
-            <div className={styles.dropdownMenu} ></div>
+            <Link className={styles.catalogLink}  href={'/catalog'} >
+              <svg width="37" height="11" viewBox="0 0 37 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line x1="36.0878" y1="1.96484" x2="11.0878" y2="1.96484" stroke="black" stroke-width="2"/>
+              <line x1="36.0878" y1="9.96484" x2="0.0877685" y2="9.96484" stroke="black" stroke-width="2"/>
+              </svg>
+                <p>Каталог</p>
+            </Link>
+              <div className={styles.dropdownMenu} ><DropdownMenu/></div>
               </li>
             <li>Мужские</li>
             <li>Женские</li>
