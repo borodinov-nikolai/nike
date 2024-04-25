77f393d649b5d186557a1a9220545df4335d5b8f
Nikolai <borodinov.nikolai@gmail.com>
2024-04-25 21:03:59 +0300

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -8,7 +8,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['https://petproekt.ru', 'https://api.petproekt.ru']});
+  app.enableCors({origin: ['https://petproekt.ru', 'https://api.petproekt.ru'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api', { exclude: ['admin*'] });
   const config = new DocumentBuilder().setTitle('nike').build();
   const document = SwaggerModule.createDocument(app, config);
