56d99bac9075dedae13485ff037976b106a39377
Nikolai <borodinov.nikolai@gmail.com>
2024-04-26 18:38:22 +0300

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
